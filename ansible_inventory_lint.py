99a775d6df84d5635341adc3111ef3569f9a267d
Your Name <you@example.com>
2022-09-02 14:31:08 +0300

    split general rules into general-all and general-line, as well as added LintInventory and testing rules on inventories.

--- ansible_inventory_lint.py
+++ ansible_inventory_lint.py
@@ -1,8 +1,35 @@
 import os
 import re
 import sys
+import mmap
 import argparse
 #TODO rules, rules exclusions, etc..
+
+
+class LintInventory:
+    def __init__(self, file):
+        self.file = file
+        self.file_dict = {}
+        self.file_str = ''
+        self.groups = {}
+        self.hosts = {}
+        self.parse_inventory()
+        self.caught_rules = []
+
+    def parse_inventory(self):
+        try:
+            with open(self.file, 'r') as f:
+                line_num = 1
+                for line in f:
+                    self.file_dict[line_num] = line
+                    if re.match('\[.*\]', line):
+                        self.groups[line_num] = line
+                    elif line.strip() != '' and '#' not in line and '=' not in line:
+                        self.hosts[line_num] = line
+                    line_num += 1
+            self.file_str = "".join(self.file_dict.values())
+        except FileNotFoundError:
+            raise FileNotFoundError(f'No such file as {self.file}')
 
 
 class LintRule:
@@ -11,17 +38,25 @@ class LintRule:
         self.level = level
         self.name = name
         self.num = num
-        self.regex = regex
+        self.regex = self.check_regex_validity(regex)
 
     def __str__(self):
         return f'{self.num}_{self.level}_{self.name}'
 
+    def check_regex_validity(self,regex):
+        """
+        :param regex: String --> Regex to test
+        :return: Boolean
+        """
+        try:
+            regex = re.compile(regex)
+        except re.error:
+            raise re.error(f'Regex passed is invalid! "{regex}" for rule {self}')
+        return regex
+
+
 def populate_rules():
-    rules_dict = {
-            'host': [],
-            'group': [],
-            'general': []
-            }
+    rules = []
     for path, folders, files in os.walk('rules'):
         if path != 'rules':
             for file in files:
@@ -31,15 +66,34 @@ def populate_rules():
                     rule_name = re.search('(?<=_).*$', file).group()
                     rule_num = re.search('(?<=\()[0-9]{3}(?=\))', file).group()
                     rule_regex = f.readline().strip()
-                    rules_dict[rule_impacts].append(LintRule(
-                                                impacts=rule_impacts,
-                                                level=rule_level,
-                                                name=rule_name,
-                                                num=rule_num,
-                                                regex=rule_regex
-                                                ))
+                    rules.append(LintRule(
+                                        impacts=rule_impacts,
+                                        level=rule_level,
+                                        name=rule_name,
+                                        num=rule_num,
+                                        regex=rule_regex
+                                        ))
+    return rules
 
-    return rules_dict
+
+def test_lint_rules(inventory, lint_rules):
+    for rule in lint_rules:
+        if rule.impacts == "host":
+            caught = list(filter(rule.regex.match, inventory.hosts.values()))
+        elif rule.impacts == "group":
+            caught = list(filter(rule.regex.match, inventory.groups.values()))
+        elif rule.impacts == "general-line":
+            caught = list(filter(rule.regex.match, inventory.file_dict.values()))
+        elif rule.impacts == "general-all":
+            caught = rule.regex.search(inventory.file_str)
+            try:
+                caught = caught.group()
+            except AttributeError:
+                pass
+        if caught:
+            inventory.caught_rules.append(rule)
+    for caught_rule in inventory.caught_rules:
+        print(caught_rule)
 
 
 def main():
@@ -52,20 +106,17 @@ def main():
                             )
     args = arg_parser.parse_args()
     path_type = check_path(args.path)
+    user_lint_rule = LintRule(
+                        impacts='host',
+                        level='ERROR',
+                        name='USER DEFINED REGEX',
+                        num='000',
+                        regex=args.regex
+                        )
+    lint_rules = populate_rules()
     if path_type == 'file':
-        rules = populate_rules()
-        for key,val in rules.items():
-            print(key,[str(rule) for rule in val])
-
-
-
-def read_inventory_file(file):
-    with open(file, 'r') as f:
-        line_num = 1
-        for line in f:
-            print(f'{line_num} {line}')
-            line_num += 1
-
+        inventory = LintInventory(args.path)
+        test_lint_rules(inventory, lint_rules)
 
 def check_path(path):
     """
